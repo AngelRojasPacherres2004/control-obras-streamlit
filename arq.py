4b9f5a0da2c2efd0c4d7d599c8d616ddabb0765d
AngelRojasPacherres2004 <142623592+AngelRojasPacherres2004@users.noreply.github.com>
2026-01-03 18:39:30 -0500

    Import authentication functions from auth module

--- arq.py
+++ arq.py
@@ -2,7 +2,7 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
 import cloudinary
-
+from auth import mostrar_pantalla_inicial, verificar_autenticacion
 # ================= INIT =================
 if not firebase_admin._apps:
     firebase_admin.initialize_app(
