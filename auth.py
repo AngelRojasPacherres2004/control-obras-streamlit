b0d776139480c33f9e0531fc9a388dac87d1fe5c
Erick-Josue <erickjosu02@gmail.com>
2026-01-28 17:26:32 -0500

    logo

--- auth.py
+++ auth.py
@@ -156,12 +156,12 @@ def verificar_autenticacion(db):
         /* ====== RESPONSIVE MÓVIL ====== */
         @media (max-width: 768px) {
             .block-container {
-                padding-top: 7vh !important;
+                padding-top: 10vh !important;
             }
 
             .login-box {
                 max-width: 90% !important;
-                padding: 15px !important;
+                padding: 20px !important;
             }
 
             .stTextInput input {
