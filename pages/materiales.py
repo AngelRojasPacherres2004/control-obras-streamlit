877df7fef841bedf8c12a3548c4f5670a4a96905
Carlosmg23 <carlosmg2394@gmail.com>
2026-02-03 18:33:44 -0500

    d

--- pages/materiales.py
+++ pages/materiales.py
@@ -378,8 +378,8 @@ if mats_obra:
     use_container_width=True,
     column_config={
         "nombre": "Material",
-        "stock_inicial": st.column_config.NumberColumn("Stock inicial", format="%.2f"),
-        "stock_actual": st.column_config.NumberColumn("Stock actual", format="%.2f"),
+        "stock_inicial": st.column_config.NumberColumn("Stock inicial", format="%.4f"),
+        "stock_actual": st.column_config.NumberColumn("Stock actual", format="%.4f"),
         "subtotal": st.column_config.NumberColumn("Inversión", format="S/ %.2f"),
     }
 )
